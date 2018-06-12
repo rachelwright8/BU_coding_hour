5863c7adc65b74c044aec0bfd2d8bf439dc658c5
rachelwright8 <rachelwright8@gmail.com>
2018-06-12 11:22:43 -0400

    first commit: README and intro2R

---
+++ README.md
@@ -0,0 +1,1 @@
+# BU_coding_hour
---
+++ intro2R_exercises_BU.R
@@ -0,0 +1,56 @@
+# Make a directory on your computer (BONUS: do this at the command line)
+# Set your working directory to that directory
+
+
+# PART 1: Analyzing Donut Consumption ----
+
+# Create a vector of 20 random numbers ranging from 1 to 10. Call it `donuts_week1`.
+
+
+# Create a vector of 20 random numbers ranging from 5 to 15. Call it `donuts_week2`.
+
+# Summarize the `donuts_week1` and `donuts_week2` objects using the `summary` function
+
+
+# What is the data type of the `donuts_week2` and `donuts_week1` objects?
+
+
+# Make a histogram of `donuts_week1`. Title the histogram "Donut Consumption at Peer Coding Hour". Call the x-axis "Number of Donuts Eaten by Peer Coders".
+
+# Plot a histogram of `donuts_week2` on top of a histogram of `donuts_week1`.
+# HINT: you may want to play around with colors here. You'll definitely need to adjust the axes to see all of the data.
+# BONUS: add a legend to the figure to discrimate between the histograms
+
+
+# Use a t-test to determine if there is a significant difference in the number of donuts consumed on week 1 vs. week 2
+
+
+# PART 2: Relationship between Donut and Coffee Consumption ----
+# Create a vector of 20 random numbers ranging from 1 to 10. Call it `coffee_week1`.
+
+
+
+# Examine this data in some way. Coders choice!
+
+
+# Create a data frame called `coffee_donut_data` that has one column for `donuts_week1` and one column for `coffee_week1`
+
+
+# Enter a line of code that tells you what type of object `coffee_donut_data` is and what type of data it contains (integers? characters?)
+
+
+# Plot donuts consumed on the x-axis and coffee consumed on the y-axis. Give the plot a name. Label the x- and y-axes.
+
+
+
+# Use a linear model to test the relationship between coffee and donut consumption. Save the results to a variable called `lm_coffee_donuts`.
+
+
+# Summarize `lm_coffee_donuts`
+
+
+# Add a regression line to the plot you made earlier using this linear model.
+
+
+
+
